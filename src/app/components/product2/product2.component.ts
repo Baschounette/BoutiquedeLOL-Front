c50948041f759ecbabb3cb15686120f4ab731fed
Newborshian <christopherbouthemy2014@gmail.com>
2023-01-31 17:57:22 +0100

    ...

--- src/app/components/product2/product2.component.ts
+++ src/app/components/product2/product2.component.ts
@@ -57,6 +57,29 @@ export class Product2Component implements OnInit {
   }
   
   addToStore(product: Product){
-    this.cookieService.set('Panier', JSON.stringify(product), {expires: 0.0021})
+    if(!this.cookieService.get('Panier')){
+      this.cookieService.set('Panier', JSON.stringify(product), {expires: 0.041})
+    } else { 
+      console.log(JSON.parse(this.cookieService.get('Panier')));
+      if(JSON.parse(this.cookieService.get('Panier'))){
+        console.log("coucou");
+        
+        let panier: Product[] = [];
+        panier.push(JSON.parse(this.cookieService.get('Panier')));
+        panier.push(product);
+        this.cookieService.delete('Panier')
+        this.cookieService.set('Panier', JSON.stringify(panier), {expires: 0.041})
+      } else {
+        console.log("Bye bye");
+        
+        let panier2: Product[] = JSON.parse(this.cookieService.get('Panier'))
+        panier2.push(product)
+        this.cookieService.delete('Panier')
+        this.cookieService.set('Panier', JSON.stringify(panier2), {expires: 0.041})
+      }
+     
+         
+    }
+
   }
 }
